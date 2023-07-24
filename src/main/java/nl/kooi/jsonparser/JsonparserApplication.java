2ed0b14983f8bfc1bf51d18ad3dbfe171b427563
lvdkooi <lvvdkooi@hotmail.com>
2023-07-24 18:07:17 +0200

    cleaning up dependencies

--- src/main/java/nl/kooi/jsonparser/JsonparserApplication.java
+++ src/main/java/nl/kooi/jsonparser/JsonparserApplication.java
@@ -1,13 +1,8 @@
 package nl.kooi.jsonparser;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class JsonparserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JsonparserApplication.class, args);
-	}
+    public static void main(String[] args) {
+    }
 
 }
