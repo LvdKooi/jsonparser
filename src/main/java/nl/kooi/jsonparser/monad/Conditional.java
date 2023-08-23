204dc02628e9ba69aa80f557673a2cfbcc1769b1
Laurens van der Kooi <laurens.van.der.kooi@ordina.nl>
2023-08-23 15:44:29 +0200

    Cleaning up, adding latest and greatest Conditional wise

--- src/main/java/nl/kooi/jsonparser/monad/Conditional.java
+++ src/main/java/nl/kooi/jsonparser/monad/Conditional.java
@@ -1,58 +1,58 @@
 package nl.kooi.jsonparser.monad;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.ArrayDeque;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Queue;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class Conditional<T, R> {
-    private final Map<Predicate<T>, Function<T, R>> actionMap;
+    private final Queue<Pair<Predicate<T>, Function<T, R>>> actionQueue;
     private final Function<T, R> currentFunction;
 
-    private Conditional(Map<Predicate<T>, Function<T, R>> actionMap, Function<T, R> currentFunction) {
-        this.actionMap = actionMap;
+    private Conditional(Queue<Pair<Predicate<T>, Function<T, R>>> actionQueue, Function<T, R> currentFunction) {
+        this.actionQueue = actionQueue;
         this.currentFunction = currentFunction;
     }
 
-    public static <T, R> Conditional<T, R> apply(Function<T, R> callable) {
-        return new Conditional<>(new LinkedHashMap<>(), callable);
+    public static <T, R> Conditional<T, R> apply(Function<T, R> function) {
+        Objects.requireNonNull(function);
+        return new Conditional<>(new ArrayDeque<>(1), function);
     }
 
     public Conditional<T, R> when(Predicate<T> condition) {
         assertCurrentFunctionAndPredicateAreValid(condition);
 
-        var map = new LinkedHashMap<>(actionMap);
-        map.put(condition, currentFunction);
+        var queue = new ArrayDeque<>(this.actionQueue);
+        queue.add(new Pair<>(condition, currentFunction));
 
-        return new Conditional<>(map, null);
+        return new Conditional<>(queue, null);
     }
 
-    private void assertCurrentFunctionAndPredicateAreValid(Predicate<T> predicate) {
-        Objects.requireNonNull(currentFunction, "A predicate can only be added after an apply(Function<T, R> callable) or orApply(Function<T, R> callable)");
-        Objects.requireNonNull(predicate, "Predicate is not nullable");
-    }
-
-    public Conditional<T, R> orApply(Function<T, R> callable) {
-        return new Conditional<>(this.actionMap, callable);
+    public Conditional<T, R> orApply(Function<T, R> function) {
+        Objects.requireNonNull(function);
+        return new Conditional<>(this.actionQueue, function);
     }
 
     public <U> Conditional<T, U> map(Function<R, U> mapFunction) {
-        var updatedMap = this.actionMap.entrySet()
+        Objects.requireNonNull(mapFunction);
+
+        var queue = this.actionQueue
                 .stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().andThen(mapFunction), (x, y) -> y, LinkedHashMap::new));
+                .map(pair -> new Pair<>(pair.key(), pair.value().andThen(mapFunction)))
+                .collect(Collectors.toCollection(ArrayDeque::new));
 
-
-        return new Conditional<>(updatedMap, null);
+        return new Conditional<>(queue, null);
     }
 
     public R applyToOrElseGet(T object, Supplier<? extends R> supplier) {
         Objects.requireNonNull(supplier);
 
-        return findMatchingFunction(object)
+        return Optional.ofNullable(object)
+                .flatMap(this::findMatchingFunction)
                 .orElseGet(() -> obj -> supplier.get())
                 .apply(object);
     }
@@ -65,11 +65,24 @@ public class Conditional<T, R> {
     }
 
     private Optional<Function<T, R>> findMatchingFunction(T t) {
-        return actionMap
-                .entrySet()
+        return actionQueue
                 .stream()
-                .filter(entry -> entry.getKey().test(t))
+                .filter(entry -> entry.key().test(t))
                 .findFirst()
-                .map(Map.Entry::getValue);
+                .map(Pair::value);
+    }
+
+    private void assertCurrentFunctionAndPredicateAreValid(Predicate<T> predicate) {
+        Objects.requireNonNull(currentFunction, "The function that belongs to this condition is not yet set. " +
+                "A predicate can only be added after an apply(Function<T, R> function) or orApply(Function<T, R> function).");
+        Objects.requireNonNull(predicate);
+    }
+
+    private record Pair<T, R>(T key, R value) {
+
+        private Pair {
+            Objects.requireNonNull(key);
+            Objects.requireNonNull(value);
+        }
     }
 }
